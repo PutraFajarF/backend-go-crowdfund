02bff6d038b6de9711bffd82578b703b9fb25cba
Putra Fajar <putrautalls@gmail.com>
2022-10-23 23:36:04 +0700

    setup project, make connection to database and get data from database by using Gorm

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"go-crowdfunding/user"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "host=localhost user=postgres password=ktl123 dbname=go_crowdfunding port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Println("Connection to database is good!")
+
+	var users []user.User
+	length := len(users)
+	fmt.Println(length)
+
+	db.Find(&users)
+
+	length = len(users)
+	fmt.Println(length)
+
+	for _, user := range users {
+		fmt.Println(user.Name)
+		fmt.Println(user.Email)
+		fmt.Println("================")
+	}
+
+}
