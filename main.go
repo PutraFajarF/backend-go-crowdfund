a58268f6e7d0dfe4db114288d3bbde2d5675403c
Putra Fajar <putrautalls@gmail.com>
2022-10-27 23:41:13 +0700

    added service for login API

--- main.go
+++ main.go
@@ -21,16 +21,19 @@ func main() {
 	userRepository := user.NewRepository(db)
 	userService := user.NewService(userRepository)
 
-	userByEmail, err := userRepository.FindByEmail("putsra@gmail.com")
+	input := user.LoginInput{
+		Email:    "putra@gmail.com",
+		Password: "password",
+	}
+
+	user, err := userService.Login(input)
 	if err != nil {
+		fmt.Println("terjadi kesalahan")
 		fmt.Println(err.Error())
 	}
 
-	if userByEmail.ID == 0 {
-		fmt.Println("User not found")
-	} else {
-		fmt.Println(userByEmail.Name)
-	}
+	fmt.Println(user.Email)
+	fmt.Println(user.Name)
 
 	userHandler := handler.NewUserHandler(userService)
 
