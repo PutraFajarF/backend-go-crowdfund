e8e0887afa191b9797aed8dbbc26de2ebbf13d02
Putra Fajar <putrautalls@gmail.com>
2022-10-29 23:32:19 +0700

    added func authMiddleware in main.go and apply it to endpoint /avatars

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"go-crowdfunding/auth"
 	"go-crowdfunding/handler"
+	"go-crowdfunding/helper"
 	"go-crowdfunding/user"
 	"log"
+	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -30,14 +34,62 @@ func main() {
 	api.POST("/users", userHandler.RegisterUser)
 	api.POST("/sessions", userHandler.Login)
 	api.POST("/email_checkers", userHandler.CheckEmailAvailability)
-	api.POST("/avatars", userHandler.UploadAvatar)
+	api.POST("/avatars", authMiddleware(authService, userService), userHandler.UploadAvatar)
 
 	router.Run()
 }
 
-// ambil nilai header Authorization: Bearer tokentokentoken
-// dari header Authorization, kita ambil nilai tokennya saja
-// kita validasi token
-// ambil user_id
-// ambil user dari db berdasarkan user_id lewat service
-// set context isinya user
+// Syarat gin handler parameter dalam fungsi hanya 1, jadi jika ada 2 sudah tidak memenuhi syarat seperti func authMiddleware(c *gin.Context, authService auth.Service)
+// Solusinya kita buat func yg akan menjalankan func (c *gin.Context) yang akan mengembalikan func handlerFunc (func yg mengembalikan *gin.Context)
+func authMiddleware(authService auth.Service, userService user.Service) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// ambil nilai header Authorization: Bearer tokentokentoken
+		// dari header Authorization, kita ambil nilai tokennya saja
+		// kita validasi token
+		// ambil user_id
+		// ambil user dari db berdasarkan user_id lewat service
+		// set context isinya user
+
+		authHeader := c.GetHeader("Authorization")
+		// cek apakah dalam string authHeader ada "Bearer"
+		if !strings.Contains(authHeader, "Bearer") {
+			response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		// Contoh : Bearer tokentokentoken => ambil tokennya saja
+		tokenString := ""
+		arrayToken := strings.Split(authHeader, " ")
+		if len(arrayToken) == 2 {
+			tokenString = arrayToken[1]
+		}
+
+		token, err := authService.ValidateToken(tokenString)
+		if err != nil {
+			response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		claim, ok := token.Claims.(jwt.MapClaims)
+		if !ok || !token.Valid {
+			response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		// ubah format dari jwt.MapClaims ke float64 lalu ubah lg ke int agar sama seperti parameter GetUserByID di service.go user
+		userID := int(claim["user_id"].(float64))
+
+		user, err := userService.GetUserByID(userID)
+		if err != nil {
+			response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		// set context yg isinya user
+		c.Set("currentUser", user)
+	}
+}
