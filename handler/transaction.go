9e93be48d5b49c94ce23562830ecf14fdb5f11dc
Putra Fajar <putrautalls@gmail.com>
2022-11-15 23:34:14 +0700

    added Handler for Transactions API

--- handler/transaction.go
+++ handler/transaction.go
@@ -1,7 +1,44 @@
 package handler
 
+import (
+	"go-crowdfunding/helper"
+	"go-crowdfunding/transaction"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 // parameter di uri
 // tangkap parameter, mapping ke input struct
 // panggil service, input struct sebagai parameter
 // service, berbekal campaign id bisa panggil repo
 // repo mencari data trasaction suatu campaign
+
+type transactionHandler struct {
+	service transaction.Service
+}
+
+func NewTransactionHandler(service transaction.Service) *transactionHandler {
+	return &transactionHandler{service}
+}
+
+func (h *transactionHandler) GetCampaignTransactions(c *gin.Context) {
+	var input transaction.GetCampaignTransactionsDetailInput
+
+	err := c.ShouldBindUri(&input)
+	if err != nil {
+		response := helper.APIResponse("Failed to get campaign's transactions", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	transactions, err := h.service.GetTransactionsByCampaignID(input)
+	if err != nil {
+		response := helper.APIResponse("Failed to get campaign's transactions", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("Campaign transactions detail", http.StatusOK, "success", transactions)
+	c.JSON(http.StatusOK, response)
+}
