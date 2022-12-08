75a75849f7b75a9ac23b236246e7a106d0a1f8e2
Putra Fajar <putrautalls@gmail.com>
2022-12-09 00:48:45 +0700

    added service for User Transaction API

--- transaction/service.go
+++ transaction/service.go
@@ -13,6 +13,7 @@ type service struct {
 
 type Service interface {
 	GetTransactionsByCampaignID(input GetCampaignTransactionsDetailInput) ([]Transaction, error)
+	GetTransactionsByUserID(userID int) ([]Transaction, error)
 }
 
 func NewService(repository Repository, campaignRepository campaign.Repository) *service {
@@ -34,6 +35,15 @@ func (s *service) GetTransactionsByCampaignID(input GetCampaignTransactionsDetai
 	}
 
 	transactions, err := s.repository.GetByCampaignID(input.ID)
+	if err != nil {
+		return transactions, err
+	}
+
+	return transactions, nil
+}
+
+func (s *service) GetTransactionsByUserID(userID int) ([]Transaction, error) {
+	transactions, err := s.repository.GetByUserID(userID)
 	if err != nil {
 		return transactions, err
 	}
