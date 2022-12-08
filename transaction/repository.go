d46ac4a34a4c9f603483fa74cf5137db8fa60431
Putra Fajar <putrautalls@gmail.com>
2022-12-08 23:55:20 +0700

    added repository for User Transaction API

--- transaction/repository.go
+++ transaction/repository.go
@@ -8,6 +8,7 @@ type repository struct {
 
 type Repository interface {
 	GetByCampaignID(campaignID int) ([]Transaction, error)
+	GetByUserID(userID int) ([]Transaction, error)
 }
 
 func NewRepository(db *gorm.DB) *repository {
@@ -24,4 +25,18 @@ func (r *repository) GetByCampaignID(campaignID int) ([]Transaction, error) {
 	}
 
 	return transactions, nil
+}
+
+func (r *repository) GetByUserID(userID int) ([]Transaction, error) {
+	var transaction []Transaction
+	// campaignImages tidak punya relasi langsung dengan Transaction
+	// Flownya dari transaction -> relasi ke campaign -> relasi ke campaignImages
+	// Caranya Preload("Campaign.CampaignImages", "campaign_images.is_primary = 1") -> load campaign dan dari campaign akan load campaignImages yg berelasi dengan campaign
+	// batasi dari campaignImages yg diload hanya is_primary = 1
+	err := r.db.Preload("Campaign.CampaignImages", "campaign_images.is_primary = 1").Where("user_id = ?", userID).Find(&transaction).Error
+	if err != nil {
+		return transaction, err
+	}
+
+	return transaction, nil
 }
